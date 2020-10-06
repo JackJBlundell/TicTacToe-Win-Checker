18dc93944d2c9610d64ad15695a99e6d94eefb79
Jack Blundell <contact@jackblundell.co.uk>
2020-10-06 12:36:13 +0100

    Check vertical first version

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-function game(board) 
+function game(board)
 {
-    check_vert(board)
-    check_hoz(board)
-    check_diag(board)
+    var vert_result = check_vert(board)
+    var hoz_result = check_hoz(board)
+    var diag_result = check_diag(board)
+
+    if(vert_result == 1 || hoz_result == 1 || diag_result == 1){
+      return "X Wins"
+    }
+    else if(vert_result == -1 || hoz_result == -1 || diag_result == -1){
+      return "O Wins"
+    }
+    else {
+      return "No Winner"
+    }
 }
 
 function check_vert(board)
@@ -10,9 +20,33 @@ function check_vert(board)
 
     for(row in board)
     {
+      var x_score = 0;
+      var o_score = 0;
         for(col in board)
         {
-
+          if(col == "x")
+          {
+           //Add one to X score for row
+            x_score += 1;
+          }
+          else if (col == "o")
+          {
+            //Add one to O score for row
+          }
+        }
+        if(x_score==3)
+        {
+          //If X has 3 in the current row
+          return -1;
+        }
+        else if (o_score==3)
+        {
+          //If O has 3 in the current row
+          return 1;
+        }
+        else
+        {
+          return 0;
         }
     }
 }

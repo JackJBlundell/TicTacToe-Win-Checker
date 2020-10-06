82014c199571540a1e26752e6c3ffedb2bbbe41f
Kyle Morris <kyle.morris@amplyfigroup.com>
2020-10-06 12:20:53 +0100

    Added Skeleton

---
+++ index.js
@@ -0,0 +1,32 @@
+function game(board) 
+{
+    check_vert(board)
+    check_hoz(board)
+    check_diag(board)
+}
+
+function check_vert(board)
+{
+
+    for(row in board)
+    {
+        for(col in board)
+        {
+
+        }
+    }
+}
+
+function check_hoz(board)
+{
+
+}
+
+function check_diag(board)
+{
+
+}
+
+const tictactoe = [ ["x",,], [,"x",], [,,"x"] ]
+
+game(tictactoe)
